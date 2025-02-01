679aaffe6de40d434a1a8b6e8f235940d5ac6b5f
Todd Dugan <tdugan@valuebasedtech.com>
2025-01-31 21:50:37 -0800

    catppuccin

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,34 +1,14 @@
 return {
   {
-    'navarasu/onedark.nvim',
-    priority = 1000, -- Ensures the colorscheme loads first
-    config = function()
-      require('onedark').setup {
-        style = 'darker', -- Options: dark, darker, cool, deep, warm, warmer
-        transparent = true,
-      }
-      require('onedark').load()
-    end,
-  },
-  {
-    'mistweaverco/retro-theme.nvim',
-    priority = 1000,
-    opts = {
-      italic_comments = true,
-      disable_cache = false,
-      hot_reload = false,
-    },
-  },
-  {
-    'folke/tokyonight.nvim',
-    priority = 1000,
+    'catppuccin/nvim',
     lazy = false,
-  },
-  {
-    'LazyVim/LazyVim',
+    name = 'catppuccin',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      require('catppuccin').setup {
+        transparent_background = true,
+      }
+      vim.cmd.colorscheme 'catppuccin-mocha'
     end,
   },
 }
