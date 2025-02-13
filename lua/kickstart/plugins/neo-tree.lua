6748277dc633b52cfb685235115a2495a381ff24
Todd Dugan <tdugan@valuebasedtech.com>
2025-02-13 09:47:54 -0800

    add shortcut to open nvim config in neotree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -12,6 +12,19 @@ return {
   cmd = 'Neotree',
   keys = {
     { '<leader>e', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    {
+      '<leader>c',
+      function()
+        require('neo-tree.command').execute {
+          action = 'focus',
+          source = 'filesystem',
+          position = 'float', -- Change to "left" if you prefer a sidebar
+          dir = vim.fn.stdpath 'config', -- Opens the Neovim config folder
+        }
+      end,
+      desc = 'Open Neovim config in NeoTree',
+      silent = true,
+    },
   },
   opts = {
     filesystem = {
