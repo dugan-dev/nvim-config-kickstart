205c1650e6f0a6eb809ff44ce6d22f14bdafd703
Todd Dugan <tdugan@valuebasedtech.com>
2025-04-22 15:52:24 -0700

    update avante config per git repo

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -2,7 +2,6 @@ return {
   {
     'yetone/avante.nvim',
     event = 'VeryLazy',
-    lazy = false,
     version = false, -- set this if you want to always pull the latest change
     opts = {
       -- add any opts here
@@ -11,6 +10,7 @@ return {
     build = 'make',
     -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
     dependencies = {
+      'nvim-treesitter/nvim-treesitter',
       'stevearc/dressing.nvim',
       'nvim-lua/plenary.nvim',
       'MunifTanjim/nui.nvim',
